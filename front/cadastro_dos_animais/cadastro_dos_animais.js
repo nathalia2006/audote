164e4149f5ff5e3d3be9c9b8c648a275d7255432
NATHALIA ORSO <04705712099@senacrs.edu.br>
2024-08-12 10:02:26 -0300

    first commit

--- front/cadastro_dos_animais/cadastro_dos_animais.js
+++ front/cadastro_dos_animais/cadastro_dos_animais.js
@@ -1,27 +1,24 @@
 async function handleSubmit(event) {
   event.preventDefault();
-
-button.onclick = async function() {
-  let email      = document.getElementById("email").value;
+  
+  let email       = document.getElementById("email").value;
   let senha      = document.getElementById("senha").value;
   let descrição_animal      = document.getElementById("descrição_animal").value;
   let foto_animal      = document.getElementById("foto_animal").value;
-  let description = document.getElementById("description").value;  
-  let data        = {email,senha,descrição_animal,foto_animal}  
 
+  let data        = {email,senha,descrição_animal,foto_animal}  
+  
   const response = await fetch('http://localhost:3005/api/cadastroAnimais/create', {
     method: "POST",
     headers: {"Content-type": "application/json;charset=UTF-8"},
     body: JSON.stringify(data)
   });
-
+  
   let content = await response.json();    
-    
+      
   if(content.success) {
-    alert("Senha correta")
-    console.log(response)
+    alert(content.message)
   } else {
     alert('Senha incorreta');
-    console.log(response)
   }
 }
