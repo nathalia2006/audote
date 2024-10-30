0dd6b99e3cf887cf86e1a000072e15a35fafbbb9
nathália <126471171+nathalia2006@users.noreply.github.com>
2024-10-30 19:55:20 -0300

    atualizando script do banco

--- banco.sql
+++ banco.sql
@@ -15,10 +15,15 @@ CREATE TABLE cadastro_animais(
 	id 	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(250) NOT NULL,
     senha VARCHAR(255) NOT NULL,
+    nome_animal VARCHAR(255) NOT NULL, 
+    telefone_animal VARCHAR(255) NOT NULL,
     descricao_animal TEXT NOT NULL,
     foto_animal TEXT NOT NULL
 );
-
 select * from usuario;
 
 select * from cadastro_animais;
+
+-- cadastro teste:
+
+INSERT INTO cadastro_animais(email, senha, nome_animal, telefone_animal, descricao_animal, foto_animal) VALUES('nathi.orso09@gmail.com', '123', 'teste', '51 99999999', 'cachorra', '1730327692613_images.jpg');
